0b0b46d67baad7310aaf48a2ec56149517f354f9
AryaGri <arinka.grishina@gmail.com>
2026-02-15 02:06:05 +0300

    Fix Render deploy - gunicorn bind port

--- my_project/settings.py
+++ my_project/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
     }
 }
 if os.environ.get('DATABASE_URL'):
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=False)
 
 
 
